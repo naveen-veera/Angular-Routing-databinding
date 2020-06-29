6baa04f08e08d928810688544030ef953572ba02
Naveen132895 <veeranaveen.sethuraman@faceacademy.in>
2020-06-29 22:14:38 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { AboutusComponent } from './aboutus/aboutus.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'databinding', component: DatabindingComponent },
+  { path: 'aboutus', component: AboutusComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
